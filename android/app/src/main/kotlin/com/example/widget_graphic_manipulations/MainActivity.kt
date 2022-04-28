1f8bb33794296848fe193046f3c516fc12261810
imashu <debug.ashu@gmail.com>
2022-04-28 19:01:24 +0530

    first comit

---
+++ android/app/src/main/kotlin/com/example/widget_graphic_manipulations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widget_graphic_manipulations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  late int _turns;
+  late double _value;
+
+  @override
+  void initState() {
+    _turns = 0;
+    _value = 0.0;
+  }
+
+  void _onChanged(double value) {
+    setState(() {
+      _value = value;
+      _turns = value.toInt();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Name here'),
+      ),
+      body: Container(
+          padding: const EdgeInsets.all(32.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Slider(
+                  value: _value,
+                  onChanged: _onChanged,
+                  min: 0.0,
+                  max: 4.0,
+                ),
+                RotatedBox(
+                  quarterTurns: _turns,
+                  child: const Text('Hello World'),
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
